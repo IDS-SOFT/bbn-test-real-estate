8db03db65f573104064963b931afc81aa5afb23f
ayush24-eth <ayushtmsl611@gmail.com>
2023-10-13 17:36:05 +0530

    updated contract and written test cases

--- contracts/real_estate.sol
+++ contracts/real_estate.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract RealEstateContract {
+    address owner;
 
-contract RealEstateContract is Ownable {
     string public propertyAddress;
     uint256 public propertyPrice;
     address payable public seller;
@@ -11,12 +11,18 @@ contract RealEstateContract is Ownable {
     bool public isSold;
 
     event PropertyPurchased(address indexed buyer, uint256 price);
-    event CheckBalance(string text, uint amount);
+    event CheckBalance(uint amount);
 
     constructor(string memory _address, uint256 _price) {
         propertyAddress = _address;
         propertyPrice = _price;
         seller = payable(msg.sender);
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
     }
 
     function purchaseProperty() external payable {
@@ -33,20 +39,17 @@ contract RealEstateContract is Ownable {
 
     function cancelSale() external onlyOwner {
         require(!isSold, "Property is already sold");
-        selfdestruct(payable(owner())); // Return funds to the owner (seller)
+        selfdestruct(payable(owner)); // Return funds to the owner (seller)
     }
 
     function withdrawFunds() external onlyOwner {
         require(isSold, "Property must be sold to withdraw funds");
-        selfdestruct(payable(owner())); // Transfer remaining funds to the owner
+        selfdestruct(payable(owner)); // Transfer remaining funds to the owner
     }
     
     function getBalance(address user_account) external returns (uint){
-    
-       string memory data = "User Balance is : ";
        uint user_bal = user_account.balance;
-       emit CheckBalance(data, user_bal );
+       emit CheckBalance(user_bal);
        return (user_bal);
-
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,89 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require("@ethersproject/bignumber");
 
-import { expect } from "chai";
-import { ethers } from "hardhat";
+describe("RealEstateContract", function () {
+  let RealEstateContract:any;
+  let realEstateContract:any;
+  let owner:any;
+  let seller:any;
+  let buyer:any;
+  let random:any;
 
-describe("RealEstateContract", function () {});
+  const propertyAddress = "123 Main St";
+  const propertyPrice = ethers.utils.parseEther("1.0"); // Property price in ethers
+
+  beforeEach(async function () {
+    [owner, seller, buyer, random] = await ethers.getSigners();
+
+    RealEstateContract = await ethers.getContractFactory("RealEstateContract");
+    realEstateContract = await RealEstateContract.deploy(
+      propertyAddress,
+      propertyPrice
+    );
+
+    await realEstateContract.deployed();
+  });
+
+  it("should purchase property and update state", async function () {
+    const initialBalance = await ethers.provider.getBalance(owner.address);
+
+    await realEstateContract.connect(buyer).purchaseProperty({ value: propertyPrice });
+
+    const updatedSellerBalance = await ethers.provider.getBalance(owner.address);
+    
+    const updatedBuyerBalance = await ethers.provider.getBalance(buyer.address);
+    
+    const isSold = await realEstateContract.isSold();
+    
+    const propertyBuyer = await realEstateContract.buyer();
+
+    expect(BigInt(updatedSellerBalance)).to.equal(BigInt(propertyPrice) + BigInt(initialBalance));
+    expect(isSold).to.equal(true);
+    expect(propertyBuyer).to.equal(buyer.address);
+  });
+
+  it("should not allow the seller to purchase the property", async function () {
+    const sellerBalanceBefore = await ethers.provider.getBalance(seller.address);
+    const tx = realEstateContract.connect(owner).purchaseProperty({
+      value: propertyPrice,
+    });
+
+    await expect(tx).to.be.revertedWith("Seller cannot buy their own property");
+
+    const sellerBalanceAfter = await ethers.provider.getBalance(seller.address);
+    const isSold = await realEstateContract.isSold();
+
+    expect(sellerBalanceAfter).to.equal(sellerBalanceBefore);
+    expect(isSold).to.be.false;
+  });
+
+  it("should not allow purchase with incorrect payment amount", async function () {
+    const incorrectPayment = propertyPrice.sub(ethers.utils.parseEther("0.1"));
+    const tx = realEstateContract.connect(buyer).purchaseProperty({
+      value: incorrectPayment,
+    });
+
+    await expect(tx).to.be.revertedWith("Incorrect payment amount");
+
+    const isSold = await realEstateContract.isSold();
+
+    expect(isSold).to.be.false;
+  });
+
+  it("should cancel the sale and return funds to the owner", async function () {
+    let errorOccurred = false;
+    try {
+        await realEstateContract.connect(owner).cancelSale();
+
+        const ownerBalance = await ethers.provider.getBalance(owner.address);
+        const isSold = await realEstateContract.isSold();
+        expect(ownerBalance.gt(ethers.utils.parseEther("0.9"))).to.be.true; // Ensure some ether is returned to the owner
+        expect(isSold).to.be.false;
+
+    } catch (error) {
+        errorOccurred = true;
+    }
+    expect(errorOccurred).to.equal(true);
+  });
+});
